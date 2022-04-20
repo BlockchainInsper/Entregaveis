d7586addfeca0ce67d4142e7a74c52ee64d2f4b1
Marlon Pereira <79490502+marlonsp@users.noreply.github.com>
2022-04-20 11:33:40 -0300

    Entrega Governance

--- 2022.1/Truffle-React-Template/src/contracts/Governance.sol
+++ 2022.1/Truffle-React-Template/src/contracts/Governance.sol
@@ -3,37 +3,48 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Coin is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        address initialAccount,
-        uint256 initialBalance
-    ) payable ERC20(name, symbol) {
-        _mint(initialAccount, initialBalance);
+    constructor() ERC20("Teste", "TXT") {
+        _mint(msg.sender,  20000000000000000000);
+    }
+    
+    function getToken() public view returns(ERC20) {
+        return this;
     }
 }
 
 contract TokenGovernance {
-
     ERC20 _token;
     uint256 public votersCounter;
     uint256 public tokenCounter;
     mapping(address => bool) whoVoted;
+    bool private _finalized;
+    bool private _teste;
 
-    constructor(ERC20 tokenCoin) {
-        _token = tokenCoin;
+    constructor(ERC20 token) {
+        _token = token;
+        votersCounter = 0;
     }
 
     function vote() public {
+        require(!_finalized);
         require(!whoVoted[msg.sender]);
         whoVoted[msg.sender] = true;
         votersCounter++;
         tokenCounter += _token.balanceOf(msg.sender);
     }
 
-    function endVote() public view returns(bool) {
+    function endVote() public {
+        require(!_finalized);
         require(votersCounter > 2);
         require(tokenCounter > 5);
-        return true;
+        _finalized = true;
+    }
+    
+    function getStatus() public view returns(bool) {
+        return _finalized;
+    }
+
+    function hasVoted(address acc) public view returns(bool) {
+        return whoVoted[acc];
     }
 }
