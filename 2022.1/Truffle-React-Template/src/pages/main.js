2131556125ae559b7c4883045db4692522708442
Marlon Pereira <79490502+marlonsp@users.noreply.github.com>
2022-04-20 11:44:14 -0300

    Entrega Governance

--- 2022.1/Truffle-React-Template/src/pages/main.js
+++ 2022.1/Truffle-React-Template/src/pages/main.js
@@ -5,29 +5,65 @@ import './main.css';
 import {
     loadWeb3,
     loadAccount,
-    loadCoin
+    loadCoin,
+    loadTokenGovernance
 } from "../helpers/web3Functions";
 
 const LandingPage = () => {
     const [account, setAccount] = useState("")
-    const [coin, setCoin] = useState("")
-    const [newToken, setNewToken] = useState("")
 
-    const callSetToken = async (nome,simbolo,qntd) => {
-     await coin.methods.constructor("teste","TXT",account,100000).send({from: account});
+    const [address, setAddress] = useState("")
+    const [ammount, setAmmount] = useState("")
+
+    const [coinBalance, setCoinBalance] = useState(0)
+
+    const [web3, setWeb3] = useState()
+
+    //Contracts
+    const [governance, setGovernance] = useState("")
+    const [coin, setCoin] = useState("")
+
+    const [hasVoted, setVoted] = useState(false)
+
+    const transfer = async(receiver, ammount) => {
+      coin.methods.transfer(receiver, web3.utils.toBN(ammount * 10 ** 18)).send({from: account});
+    }
+
+    const vote = async() => {
+      governance.methods.vote().send({from : account});
+    }
+
+    const endVote = async() => {
+      governance.methods.endVote().send({from : account});
     }
 
     const loadBlockchainData = async () => {
         var web3 = await loadWeb3();
+        setWeb3(web3);
         const networkId = await web3.eth.net.getId()
         const acc = await loadAccount(web3);
+
         const coinContract = await loadCoin(web3, networkId);
         if(!coinContract) {
           window.alert('Smart contract not detected on the current network. Please select another network with Metamask.')
           return;
         }
+
+        const tokenGovernance = await loadTokenGovernance(web3, networkId);
+        if(!tokenGovernance) {
+          window.alert('aSmart contract not detected on the current network. Please select another network with Metamask.')
+          return;
+        }
+
+        const voted = await tokenGovernance.methods.hasVoted(acc).call()
+        setVoted(voted)
+        
+        const bal = await coinContract.methods.balanceOf(acc).call()
+        setCoinBalance(bal)
+
         setAccount(acc);
         setCoin(coinContract);
+        setGovernance(tokenGovernance)
       }
     useEffect(() => {
       loadBlockchainData();
@@ -41,39 +77,24 @@ const LandingPage = () => {
             <div className="col">
               <div className="row">
                 <div>
-                  <p className='title'>account:</p>
-                  <div>{account &&
-                    `${account.slice(0, 6)}...${account.slice(
-                      account.length - 4,
-                      account.length
-                    )}`}
-                </div>
+                  <p className='title'>Coin Balance</p>
+                  <div>{parseInt(coinBalance)/10**18}</div>
                 </div>
               </div>
                 <div className="row">
-                  <div>
-                  </div>
+                  <p className='title'>Transfer</p>
+                  <input placeholder='receiver' value={address} onChange = {(e) => setAddress(e.target.value)} />
+                  <input placeholder='ammount' value={ammount} onChange = {(e) => setAmmount(e.target.value)} />
+                  <button className='btn' type="button" onClick={() => transfer(address, ammount)}> transfer</button>
                 </div>
-                <div className="col">
-                  <div className="row">
-                    <div>
-                      <p className='title'> Novo nome:</p>
-                      <input placeholder="Nova nome" className='inp' onChange={(nome) => setNewToken(nome.target.value)}/>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div>
-                      <p className='title'> Novo simbolo:</p>
-                      <input placeholder="Nova simbolo" className='inp' onChange={(simbolo) => setNewToken(simbolo.target.value)}/>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div>
-                      <p className='title'> Qntd inicial:</p>
-                      <input placeholder="Qntd inicial" className='inp' onChange={(qntd) => setNewToken(qntd.target.value)}/>
-                    </div>
-                      <button className='btn' type="button" onClick={() => callSetToken(newToken)}>Submit</button>
-                  </div>
+                <div className="row">
+                  <p className='title'> vote</p>
+                  <div>{hasVoted ? "already voted" : "has not voted"}</div>
+                  <button className='btn' type="button" onClick={vote}> vote</button>
+                </div>
+                <div className="row">
+                  <p className='title'> end vote</p>
+                  <button className='btn' type="button" onClick={endVote}>END</button>
                 </div>
             </div>
           </div>
@@ -81,5 +102,7 @@ const LandingPage = () => {
       </div>
     )
 }
+
+export default LandingPage;
 
 export default LandingPage;
