5236e634b4b4ae814825feacf7502b0c8326d8f3
Marlon Pereira <79490502+marlonsp@users.noreply.github.com>
2022-04-20 11:45:51 -0300

    Entrega Governance

--- 2022.1/Truffle-React-Template/migrations/2_deploy_contracts.js
+++ 2022.1/Truffle-React-Template/migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const Coin = artifacts.require("Coin");
 const TokenGovernance = artifacts.require("TokenGovernance");
 
-module.exports = function (deployer, network, accounts) {
-  const deployerAddress = accounts[0]; 
-  deployer.deploy(Coin, "Teste", "TXT", deployerAddress, 20000000000000);
-  // deployer.deploy(TokenGovernance);
+module.exports = async function (deployer) {
+  deployer.deploy(Coin).then(function() {
+    return deployer.deploy(TokenGovernance, Coin.address);
+  });
 };
