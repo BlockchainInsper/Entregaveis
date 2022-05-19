71512c37ede3c6fbd490b020359c1aab41647a8d
GustavoMBorges <62896418+GustavoMBorges@users.noreply.github.com>
2022-05-19 00:34:02 -0300

    Correcao

--- 2022.1/GustavoBorges/ERC721  Capped /ERC721Capped-GustavoBorges.sol
+++ 2022.1/GustavoBorges/ERC721  Capped /ERC721Capped-GustavoBorges.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
