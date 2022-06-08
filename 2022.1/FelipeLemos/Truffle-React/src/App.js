f2c211b9c0e0fb4f3fee9c94fc133bff5e023954
felipebpl <felipebpl@al.insper.edu.br>
2022-06-07 23:52:39 -0300

    entrega truffle-react

---
+++ 2022.1/FelipeLemos/Truffle-React/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import LandingPage from './pages/main';
+
+function App() {
+
+  return (
+    <div>
+      <LandingPage />
+    </div>
+
+  );
+}
+
+export default App;
