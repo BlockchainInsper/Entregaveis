a68ec9516abb7fa38addf0f2730f0b318e24e0da
Bruno Boldrim Saboya <62663074+BrunoSaboya@users.noreply.github.com>
2022-09-02 20:41:20 -0300

    Entrega 2 - Cofre

---
+++ 2022.2/BrunoSaboya/Entrega2/Open_cofre.py
@@ -0,0 +1,11 @@
+from web3 import Web3, HTTPProvider
+
+end="0x512e1a4c24677fe69b8B007763a5A64a2291f499"
+
+rede="https://rinkeby.infura.io/v3/5d099bf9f1a14ae39b7d2fd3c6313ee2"
+
+connection=Web3(HTTPProvider(rede))
+
+storage=connection.eth.get_storage_at(end,1).hex()
+
+print(storage)
